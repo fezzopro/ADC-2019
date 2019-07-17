b2b9ff66f4384461b5ecff74ef160e2fab2c3b59
KAMANA Felix <fezzopro@gmail.com>
2019-07-17 11:38:51 +0200

    Adding neccessary functions into our WayFarer class to hundle the required operations

--- app.js
+++ app.js
@@ -4,5 +4,13 @@ class WayFarer{
     add(a, b){
         return a+b;
     }
+    signin(username, password){}
+    signup(params = []){}
+    createTrip(params = []){}
+    cancelTrip(userId = this.userId, tripId){}
+    viewTrip(userId = this.userId, tripId=all){}
+    BookTrip(userId = this.userId, params = []){}
+    viewBooking(bookingId = all){}
+    deleteBooking(userId = this.userId, bookingId){}
 }
 module.exports = new WayFarer();
