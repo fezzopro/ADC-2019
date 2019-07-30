4c93264c88f9f1ba8b847dee8f65fb2849e0ceea
fezzopro <fezzopro@gmail.com>
2019-07-30 14:17:16 +0200

    save changes

--- server.js
+++ server.js
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-const Express = require("express");
-const WayFarer = require("./server/app");
-
-const app = new Express();
-
-// Creating and points
-app.get("/", (request, response) =>{
-    response.status(200).send("./index.html");
-});
-app.get("/index", (request, response) =>{
-
-});
-app.get("/signin", (request, response) =>{
-
-});
-app.get("/sign-up", (request, response) =>{
-
-});
-app.get("/home", (request, response) =>{
-
-});
-app.get("/accountinfo", (request, response) =>{
-
-});
-app.get("/allbooking", (request, response) =>{
-
-});
-app.get("/booking", (request, response) =>{
-
-});
-app.get("/createtrips", (request, response) =>{
-
-});
-app.get("/canceltrips", (request, response) =>{
-
-});
-app.get("/alltrips", (request, response) =>{
-
-});
-app.get("/documentations", (request, response) =>{
-
-});
-app.get("/logout", (request, response) =>{
-
-});
-// Starting the server
-const server = app.listen(8000, () =>{
-    console.log(`Listening on port ${server.address().port}...`);
-=======
 const Express = require("express");
 const path = require("path");
 const WayFarer = require("./server/app");
@@ -146,5 +96,4 @@ app.get("/logout/", (request, response) =>{
 const server = app.listen(8000, () =>{
     console.log(`Listening on port ${server.address().port}...`);
     // console.log(__dirname, __filename);
->>>>>>> b3f6f718bea74a83a2eab5b805c7080a76d8999c
 });
