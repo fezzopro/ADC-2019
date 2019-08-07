0a35701018b8a27e4717d9de473183257cc9fd4a
fezzopro <fezzopro@gmail.com>
2019-08-07 12:28:23 +0200

    Working on tests

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,51 +1,66 @@
+const request = require("request");
 const app = require("../server/controller/wayfarer");
+const routes = require("../server");
 
-describe("Addition Function", ()=>{
-    describe("Functions Related To User Authentication",()=>{
+describe("SERVER", () => {
+    let server;
+    beforeAll(() => {
+        server = require("../server")
+    });
+    afterAll(()=>{
+        server.close();
+    });
+});
+describe("/auth/signin",()=>{
+
+});
+describe("Addition Function", () => {
+
+    describe("Functions Related To User Authentication", () => {
         let username = 'fezzopro@gmail.com';
         let password = 'password';
-        it("Check Sign-in information", ()=>{
-            expect(app.signin(username,password)).toBe(true);
+        it("Check Sign-in information", () => {
+            expect(app.signin(username, password)).toBe(true);
         });
-        it("Check Sign-in information", ()=>{
+        it("Check Sign-in information", () => {
             expect(password.length > 3).toBe(true);
         });
 
-        it("Check Sign-up information", ()=>{
-            expect(app.signup(firstname,lastname,email,dob,username,password)).toBe(true);
+        it("Check Sign-up information", () => {
+            expect(app.signup(firstname, lastname, email, dob, username, password)).toBe(true);
         });
 
-        it("Check Logout function", (userId=10)=>{
+        it("Check Logout function", (userId = 10) => {
             expect(app.logout(userId)).toBe(true);
         });
     });
 
-    describe("Functions Related To Trips",()=>{
+    describe("Functions Related To Trips", () => {
 
-        it("Check create Trip function", ()=>{
-            expect(app.createTrip(tripname, departuretime,origin,destination,numb_of_seats,bus_plate)).toBe(true);
+        it("Check create Trip function", () => {
+            expect(app.createTrip(tripname, departuretime, origin, destination, numb_of_seats, bus_plate)).toBe(true);
         });
 
-        it("Check cancel Trip function", ()=>{
+        it("Check cancel Trip function", () => {
             expect(app.cancelTrip(userId, tripId)).toBe(true);
         });
 
-        it("Check view Trips function", ()=>{
+        it("Check view Trips function", () => {
             expect(app.viewTrip(userId, tripId)).toBe(true);
         });
     })
 
-    describe("Functions Related To Bookings",()=>{
+    describe("Functions Related To Bookings", () => {
 
-        it("Check book Trips function", ()=>{
+        it("Check book Trips function", () => {
             expect(app.BookTrip(userId, tripId)).toBe(true);
         });
-    
-        it("Check view booking function", ()=>{
+
+        it("Check view booking function", () => {
             expect(app.viewBooking(userId, bookingId)).toBe(true);
         });
-    
-        it("Check delete booking function", ()=>{
+
+        it("Check delete booking function", () => {
             expect(app.deleteBooking(userId, bookingId)).toBe(true);
         });
     });
